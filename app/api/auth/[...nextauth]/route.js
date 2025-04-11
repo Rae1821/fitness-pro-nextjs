a332665aff7a8005c923e91e198cd839bbb05e29
Rachel Dooley <rae1821@gmail.com>
2025-04-11 12:55:08 -0400

    Fixing linting errors

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,53 +1,51 @@
-import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-import { connectToDB } from '@utils/database';
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { connectToDB } from "@utils/database";
 
-import User from '@models/user';
+import User from "@models/user";
 
 const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        })
-    ],
-    callbacks: {
-        async session({ session }) {
-            const sessionUser = await User.findOne({
-                email: session.user.email
-            })
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
+      });
 
-            session.user.id = sessionUser._id.toString();
+      session.user.id = sessionUser._id.toString();
 
-            return session;
-        },
-        async signIn({ profile }) {
-            try {
-                //serverless Lambda function opens only when it gets called - don't have to keep server running constantly just have to connect to database
-                await connectToDB();
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        // serverless Lambda function opens only when it gets called - don't have to keep server running constantly just have to connect to database
+        await connectToDB();
 
-                //check if a user already exists
-                const userExists = await User.findOne({
-                    email: profile.email
-                });
-                //if not, create a new user and save to database
-                if(!userExists) {
-                    await User.create({
-                        email: profile.email,
-                        username: profile.name.replace(" ", "").toLowerCase(),
-                        image: profile.picture
-                    })
-                }
-
-                return true
-            } catch(error) {
-                console.log("Error checking if user exists: ", error.message);
-                return false;
-            }
+        // check if a user already exists
+        const userExists = await User.findOne({
+          email: profile.email,
+        });
+        // if not, create a new user and save to database
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
         }
-    }
 
-
-})
+        return true;
+      } catch (error) {
+        console.log("Error checking if user exists: ", error.message);
+        return false;
+      }
+    },
+  },
+});
 
 export { handler as GET, handler as POST };
--- app/api/workout/[id]/route.js
+++ app/api/workout/[id]/route.js
@@ -1,72 +1,79 @@
-import { connectToDB } from '@utils/database';
-import Workout from '@models/workout';
+import { connectToDB } from "@utils/database";
+import Workout from "@models/workout";
 
 export const GET = async (request, { params }) => {
-    try {
-        await connectToDB();
+  try {
+    await connectToDB();
 
-        const workout = await Workout.findById(params.id).populate('creator');
+    const workout = await Workout.findById(params.id).populate("creator");
 
-        if(!workout) return new Response("workout not found", { status: 404 });
+    if (!workout) return new Response("workout not found", { status: 404 });
 
-        return new Response(JSON.stringify(workout), { status: 200 })
-
-    } catch(error) {
-        return new Response("Failed to fetch all workouts", { status: 500 })
-    }
-}
+    return new Response(JSON.stringify(workout), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to fetch all workouts", { status: 500 });
+  }
+};
 
 export const PATCH = async (request, { params }) => {
-    const { date, workoutName, workoutFocus, tag, instructor, time, intervals, speedHigh, speedLow, incline, duration, distance, exerciseObj } = await request.json();
+  const {
+    date,
+    workoutName,
+    workoutFocus,
+    tag,
+    instructor,
+    time,
+    intervals,
+    speedHigh,
+    speedLow,
+    incline,
+    duration,
+    distance,
+    exerciseObj,
+  } = await request.json();
 
-    try {
-        await connectToDB();
+  try {
+    await connectToDB();
 
-        //find the existing workout by id
-        const existingWorkout = await Workout.findById(params.id);
+    // find the existing workout by id
+    const existingWorkout = await Workout.findById(params.id);
 
-        if(!existingWorkout) {
-            return new Response("Workout not found", { status: 404 });
-        }
-
-        //update the workout with new data
-        existingWorkout.date = date;
-        existingWorkout.workoutName = workoutName;
-        existingWorkout.workoutFocus = workoutFocus;
-        existingWorkout.tag = tag;
-        existingWorkout.instructor = instructor;
-        existingWorkout.time = time;
-        existingWorkout.intervals = intervals;
-        existingWorkout.speedHigh = speedHigh;
-        existingWorkout.speedLow = speedLow;
-        existingWorkout.incline = incline;
-        existingWorkout.duration = duration;
-        existingWorkout.distance = distance;
-        existingWorkout.exerciseObj = exerciseObj;
-
-
-        await existingWorkout.save();
-
-        return new Response("Successfully updated workout", { status: 200 });
-
-
-    } catch(error) {
-        return new Response("Failed to update workout", { status: 500 });
+    if (!existingWorkout) {
+      return new Response("Workout not found", { status: 404 });
     }
-}
+
+    // update the workout with new data
+    existingWorkout.date = date;
+    existingWorkout.workoutName = workoutName;
+    existingWorkout.workoutFocus = workoutFocus;
+    existingWorkout.tag = tag;
+    existingWorkout.instructor = instructor;
+    existingWorkout.time = time;
+    existingWorkout.intervals = intervals;
+    existingWorkout.speedHigh = speedHigh;
+    existingWorkout.speedLow = speedLow;
+    existingWorkout.incline = incline;
+    existingWorkout.duration = duration;
+    existingWorkout.distance = distance;
+    existingWorkout.exerciseObj = exerciseObj;
+
+    await existingWorkout.save();
+
+    return new Response("Successfully updated workout", { status: 200 });
+  } catch (error) {
+    return new Response("Failed to update workout", { status: 500 });
+  }
+};
 
 export const DELETE = async (request, { params }) => {
+  try {
+    await connectToDB();
 
-    try {
-        await connectToDB();
+    // find the workout by id and remove it
+    await Workout.findByIdAndRemove(params.id);
 
-        //find the workout by id and remove it
-        await Workout.findByIdAndRemove(params.id);
-
-        return new Response("Workout deleted", { status: 200 });
-
-    } catch(error) {
-        return new Response("Failed to delete workout", { status: 500 })
-    }
-
-}
+    return new Response("Workout deleted", { status: 200 });
+  } catch (error) {
+    return new Response("Failed to delete workout", { status: 500 });
+  }
+};
